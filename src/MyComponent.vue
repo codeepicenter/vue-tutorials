816fe10d11854499727697be3822b158ded98158
Amir Duran <amir.duran@gmail.com>
2019-11-03 13:06:57 +0100

    [SECTION-1] - Create new project

---
+++ src/MyComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+    <h2>This is our first component</h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style scoped>
+</style>
